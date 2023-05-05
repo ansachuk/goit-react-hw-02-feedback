f9c81f86ff63777a6cd292585a559cf8457b307e
ansachuk <ansachuk@icloud.com>
2023-05-05 23:25:48 +0300

    fix smth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ export default class App extends Component {
   };
 
   onControlClick = title => {
-    Notify.info('Thank you for feedback!');
+    Notify.info('Thank for your feedback!');
     this.setState(prevState => {
       return {
         [title]: prevState[title] + 1,
